2a73eed520312d5691d0a9a12676b901ccbcdabc
Diogo Pinto <diogosmp@gmail.com>
2020-02-18 03:00:07 +0000

    Implemented DELETE functionality

--- express_server.js
+++ express_server.js
@@ -37,28 +37,31 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Main route: page displays all urls in the "database"
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase }; // this is sending the urlDatabase object to the EJS template - it needs to be an object, even if it's a single variable, so that we can use its key to access the data within our template
   res.render("urls_index", templateVars); // This refers to the template './views/urls_index.ejs'. By default EJS automatically looks into the views directory for .ejs files
 });
 
+// Render the page for adding a new URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Handles the POST request that adds new URL to the "database"
 app.post("/urls", (req, res) => {
   // Tests if we have a valid URL and redirects to a 404 if not
   // For now we are not rendering a specific page or doing client side validation for the URL
   if (!isValidURL(req.body.longURL)) {
     res.redirect();
   }
-
   // Generates a new shortURL and stores it in the object
   const newShortUrl = generateRandomString();
   urlDatabase[newShortUrl] = req.body.longURL;
   res.redirect(`/urls/${newShortUrl}`);
 });
 
+// Renders the page that display the requested shorted URL
 app.get("/urls/:shortURL", (req, res) => {
   // test with object shorthand notation
   let templateVars = {
@@ -66,6 +69,12 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
+});
+
+// Removes posted URL from "database"
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
 
 // Redirects user to the longURL stored in our objects
